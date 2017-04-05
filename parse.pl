eb7c9c914db80b6f2c580ca56827b4952cbc05ba
Dominic Humphries <dominich@broadbean.com>
2017-04-05 11:05:32 +0100

    Move logic to package

--- parse.pl
+++ parse.pl
@@ -8,64 +8,11 @@ $Data::Dumper::Sortkeys = 1;
 $Data::Dumper::Indent = 1;
 $Data::Dumper::Maxdepth = 2;
 
-sub tokens {
-    my $input = shift;
-    my $building_string = 0;
-    my $string_escape = 0;
-    my $string = '';
-    return sub {
-        TOKEN: {
-          if ($building_string) {
-            if ($input =~ /\G " /gcx) {
-                if ($string_escape) {
-                    $string .= '"';
-                    $string_escape = 0;
-                    redo TOKEN
-                }
-                else {
-                    my $completed_string = $string;
-                    $string = '';
-                    $building_string = 0;
-                    return ['string', $completed_string]
-                }
-            }
-            if ($input =~ /\G \\  /gcx) {
-                $string_escape = 1;
-                redo TOKEN
-            }
-            if ($input =~ /\G (.) /gcx) {
-                if ($string_escape) {
-                    $string .= '\\';
-                    $string_escape = 0;
-                }
-                $string .= $1;
-                redo TOKEN
-            }
-          }
-          else {
-            if ($input =~ /\G \( /gcx) {
-                return ['open-paren']
-            }
-            if ($input =~ /\G \) /gcx) {
-                return ['close-paren']
-            }
-            if ($input =~ /\G " /gcx) {
-                $building_string = 1;
-                redo TOKEN
-            }
-            if ($input =~ /\G \s+ /gcx) {
-                redo TOKEN
-            }
-            if ($input =~ /\G ([^"() ]+) /gcx) {
-                return ['symbol', $1]
-            }
-            return;
-          }
-        }
-    }
-}
+use lib 'lib';
+use Parse;
 
-my $thing = tokens('(  (abc de    "f g" h)  )');
+
+my $thing = Parse::tokens('(  (abc de    "f g" h)  )');
 while (1) {
     my $foo = $thing->();
     last unless $foo;
